354c75a51848047818ee8d16f8c0d86be52b2823
Rodrigo Henriky <65931987+rhenriky@users.noreply.github.com>
2024-03-01 19:15:50 +0000

    Lista Generalizada

---
+++ GeneralizedLinkedList.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "GeneralizedLinkedList.h"
+
+
+int addAtom(Node **list, int atom){
+    Node *no = (Node*)malloc(sizeof(Node));
+    if(no == NULL) return -1;
+    no->type = 0;
+    no->
+}
+
