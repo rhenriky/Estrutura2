15ecafe440ff43c66b845878ff7f26aabb940c08
Rodrigo Henriky <65931987+rhenriky@users.noreply.github.com>
2024-03-22 22:55:56 +0000

    Tabale hash com mais usuario

--- HashTest.c
+++ HashTest.c
@@ -20,8 +20,11 @@ bool compararChaves(void * data,void *key){
 
 void imprimirUser(void * user){
     userBanco * user1 = (userBanco*) user;
-    printf("Nome:%s e Saldo:%.2f\n", user1->nome,user1->saldo);    
+    printf("Nome:%s e Saldo:%.2f\n", user1->nome,user1->saldo);
+  
 }
+
+
 
 int main() {
     
@@ -33,13 +36,26 @@ int main() {
     userBanco usuario1 = {"Joao", 100.0};
     userBanco usuario2 = {"Maria", 200.0};
     userBanco usuario3 = {"Carlos", 150.0};
-
+    userBanco usuario4 = {"Henrique", 350.0};
+    userBanco usuario5 = {"Pedro", 700.0};
+    userBanco usuario6 = {"Rafael", 440.0};
+    userBanco usuario7 = {"Lucas", 850.0};
+    userBanco usuario8 = {"Rodrigo", 350.0};
+    userBanco usuario9 = {"Bianca", 100.0};
+    
     put(&Hash, usuario1.nome, &usuario1, compararChaves);
     put(&Hash, usuario2.nome, &usuario2, compararChaves);
     put(&Hash, usuario3.nome, &usuario3, compararChaves);
+    put(&Hash, usuario4.nome, &usuario4, compararChaves);
+    put(&Hash, usuario5.nome, &usuario5, compararChaves);
+    put(&Hash, usuario6.nome, &usuario6, compararChaves);
+    put(&Hash, usuario7.nome, &usuario7, compararChaves);
+    put(&Hash, usuario8.nome, &usuario8, compararChaves);
+    put(&Hash, usuario9.nome, &usuario9, compararChaves);
+    
 
     // Busca por um usuário
-    char *nomeBuscado = "Maria";
+    char *nomeBuscado = "Carlos";
     userBanco *usuarioBuscado = get(&Hash, nomeBuscado, compararChaves);
     if (usuarioBuscado != NULL) {
         printf("Usuário %s encontrado. Saldo: %.2f\n", nomeBuscado, usuarioBuscado->saldo);
